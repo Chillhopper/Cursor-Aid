ceeeacd8baa209a6ef03e76aca76bbbd9fe68678
Chillhopper <68851163+Chillhopper@users.noreply.github.com>
2023-06-11 13:42:28 +0800

    Move mouse with eyes

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import cv2
+import numpy as np
 
 cam = cv2.VideoCapture(0)
 
 while True:
     check, frame = cam.read()
     cv2.imshow('vedio', frame)
+    grey_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
     key = cv2.waitKey(1)
     if key == 27:
         cam.release()
